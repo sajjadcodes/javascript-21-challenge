92969deda397de0b6c3256d8fb911366a80278bb
sajjadcodes <sakraan01@gmail.com>
2022-07-19 07:03:00 +0500

    day#01 challenge#01

---
+++ Day#01/challenge#sweap-variables.js
@@ -0,0 +1,16 @@
+
+function test() {
+    var a ="2";
+    var b = "8";
+
+    // write code here
+
+    var c = a;
+    a = b;
+    b = c;
+
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+
+}
